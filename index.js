72fc8dcfc1c6e290c4d697f02bf82669268eb88d
Arup Raut <arupraut2011@gmail.com>
2023-02-14 21:56:52 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const port = 3000
+var bodyParser = require('body-parser')
+var cors = require('cors');
+const http = require('http');
+
+const corsOpts = {
+  origin: '*',
+  methods: [
+    'GET',
+    'POST',
+    'DELETE'
+  ],
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+app.use(cors(corsOpts));
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+app.get('/', function(req, res){
+    res.send("App is running");
+})
+
+app.get("/test", (req, res)=>{
+        console.log("This from test module")
+        res.send("Test module")
+})
+
+app.use(() =>{
+    console.log("App is runing...")
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

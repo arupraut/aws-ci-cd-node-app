8aeb89431e64a33c188be713e018e05ac841b940
Arup Raut <arupraut2011@gmail.com>
2023-02-17 14:58:38 +0530

    updated

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -6,5 +6,13 @@ cd /home/ubuntu/workspace/aws-ci-cd-node-app
 #_Delete_Old_PM2_Service
 #sudo pm2 delete Backend
 #sudo pm2 start server.js --name Backend
-pm2 delete Backend
+#pm2 delete Backend
+#pm2 start index.js --name Backend
+
+# add npm and node to path
+export NVM_DIR = "$HOME/.nvm"
+[ -s "$NVM_DIR/.nvm.sh" ] && \. "$NVM_DIR/.nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+npm install
 pm2 start index.js --name Backend

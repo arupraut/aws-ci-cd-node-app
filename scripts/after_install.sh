d9c5443d9abad1de200202059eaa91c35a7bb664
Arup Raut <arupraut2011@gmail.com>
2023-02-17 12:35:45 +0530

    updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -6,11 +6,11 @@ sudo chmod -R 777 /home/ubuntu/workspace/aws-ci-cd-node-app
 cd /home/ubuntu/workspace/aws-ci-cd-node-app
 
 #_Remove_Unused_Code
-# rm -rf node_modules
+rm -rf node_modules
 #rm -rf build
 
-sudo chmod g+s /home/ubuntu/workspace/aws-ci-cd-node-app
-sudo chmod o-rwx /home/ubuntu/workspace/aws-ci-cd-node-app
+# sudo chmod g+s /home/ubuntu/workspace/aws-ci-cd-node-app
+# sudo chmod o-rwx /home/ubuntu/workspace/aws-ci-cd-node-app
 
 
 #Install_node_modules_&_Make_React_Build

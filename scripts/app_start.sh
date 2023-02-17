129105b7e7f26ddef97b2260e4b920110556b114
Arup Raut <arupraut2011@gmail.com>
2023-02-17 18:44:59 +0530

    updated

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -22,7 +22,7 @@ export NVM_DIR="$HOME/.nvm"
 #sudo ln -s /usr/local/bin/node-waf /usr/bin/node-wa
 
 
-sudo npm install
+npm install
 # sudo /usr/local/bin/node /home/ubuntu/workspace/aws-ci-cd-node-app/server.js
 
 pm2 start index.js --name Backend
